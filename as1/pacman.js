627e852cf325496ecabe903f9da01584f9531a91
Hyeonyoung <phyeony@gmail.com>
2023-06-22 23:53:35 -0700

    big progress. good work.

--- as1/pacman.js
+++ as1/pacman.js
@@ -1,35 +1,58 @@
+// TODO:
+// 1. Ghost movement
+// 2. Ghost collision (game over screen)
+// 3. HTML Score display
+// 4. Ghost home boundary display
+// 5. Game win screen
+// 6. setEventListeners on windows?
+// 7. Read over assingnment requirements
+
 const WALL = 0;
 const DOT = 1;
 const EMPTY = 2;
 const PACMAN = 3;
 const R_GHOST = 4;
-const T_GHOST = 5;
+const B_GHOST = 5;
 
-const tileSize = 20;
-const tileWidth = 50;
-const tileHeight = 50;
-// const tileWidth2CanvasWidthRatio = tileWidth/canvas.width;
-// const tileHeight2CanvasHeightRatio = tileHeight/canvas.height;
+const NUM_ROW = 10;
+const NUM_COL = 9;
 
-const GREEN = [0,1,0,1]
-const DARK_YELLOW = [0.6, 0.6, 0, 1];
-const TEAL_BLUE = [0, 0.5, 0.5, 1];
-const RED = [1, 0, 0, 1];
-const BLUE = [0, 0, 1, 1];
+var tileWidth = 2 / NUM_COL; // clip space [-1,1] 
+var tileHeight = 2 / NUM_ROW;
 
+const GREEN = [0,1,0]
+const DARK_YELLOW = [0.7, 0.7, 0];
+const TEAL_BLUE = [0,1,1];
+const RED = [1, 0, 0];
+const BLUE = [0, 0, 1];
+const GREY = [0.8, 0.8, 0.8];
 
-const mapState = [
+const LEFT = "ArrowLeft";
+const RIGHT = "ArrowRight";
+const DOWN = "ArrowDown";
+const UP = "ArrowUp";
+
+// initial position
+// [row, col]
+var pacmanPosition = [9, 4];
+var rGhostPosition = [4, 4];
+var bGhostPosition = [5, 4];
+
+var score =0;
+
+var mapState = [
     [DOT, DOT,  DOT,  DOT,   DOT,   DOT,  DOT,  DOT,  DOT,],
     [DOT, WALL, WALL, WALL,  DOT,   WALL, WALL, WALL, DOT,],
     [DOT, WALL, WALL, WALL,  DOT,   WALL, WALL, WALL, DOT,],
     [DOT, DOT,  DOT,  DOT,   DOT,   DOT,  DOT,  DOT,  DOT,],
     [DOT, WALL, DOT,  DOT, R_GHOST, DOT,  DOT,  WALL, DOT,],
-    [DOT, WALL, DOT,  DOT, T_GHOST, DOT,  DOT,  WALL, DOT,],
+    [DOT, WALL, DOT,  DOT, B_GHOST, DOT,  DOT,  WALL, DOT,],
     [DOT, DOT,  DOT,  DOT,   DOT,   DOT,  DOT,  DOT,  DOT,],
     [DOT, WALL, WALL, WALL,  DOT,   WALL, WALL, WALL, DOT,],
     [DOT, WALL, WALL, WALL,  DOT,   WALL, WALL, WALL, DOT,],
     [DOT, DOT,  DOT,  DOT,  PACMAN, DOT,  DOT,  DOT,  DOT,],
 ]
+console.log(mapState[9][4])
 // Refered https://github.com/davidwparker/programmingtil-webgl for various drawings
 
 /*
@@ -62,7 +85,7 @@ function initializeContext() {
     // TODO: Set the line width to 1.0.
     gl.lineWidth(1.0);
 
-    logMessage("WebGL initialized.");
+    console.log("WebGL initialized.");
 }
 
 function drawCircle(radius, centerX, centerY, color, vertexBuffer, colorBuffer) {
@@ -115,39 +138,55 @@ function drawCustomTriangle(centerX, centerY, width, height, color, vertexBuffer
     // console.log(rectangleColor)
     drawA(gl.TRIANGLE_STRIP, triangleVertices, triangleColor, vertexBuffer, colorBuffer)
 }   
- // function drawCircle() {
-//     // Create a buffer object
-//     var vertexBuffer = gl.createBuffer(),
-//         vertices = [],
-//         vertCount = 2;
-//     for (var i=0.0; i<=360; i+=1) {
-//       // degrees to radians
-//       var j = i * Math.PI / 180;
-//       // X Y Z
-//       var vert1 = [
-//         Math.sin(j),
-//         Math.cos(j),
-//       ];
-//       var vert2 = [
-//         0, // center X
-//         0, // center Y
-//       ];
-//       // DONUT:
 
-//       vertices = vertices.concat(vert1);
-//       vertices = vertices.concat(vert2);
-//     }
-//     var n = vertices.length / vertCount;
-//     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-//     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+function checkCollision(pacmanDirection) {
+    var [pacmanRow, pacmanCol] = pacmanPosition
+    var nextRow = pacmanRow
+    var nextCol = pacmanCol
+    
+    if(pacmanDirection === UP) {
+        nextRow -= 1
+    } else if(pacmanDirection === DOWN) {
+        nextRow += 1
+    } else if(pacmanDirection === LEFT) {
+        nextCol -= 1
+    } else if(pacmanDirection === RIGHT) {
+        nextCol += 1
+    }
+    if(nextCol < 0 || nextCol > 8 || nextRow < 0 || nextRow > 9) {
+        console.log("OUT OF BOUNDS")
+        return;
+    }
+    if(mapState[nextRow][nextCol] === WALL) {
+        console.log("WALL")
+        return
+    } else if(mapState[nextRow][nextCol] === DOT) {
+        mapState[pacmanRow][pacmanCol] = EMPTY
+        score += 100
+        console.log(score)
+    } else if (mapState[nextRow][nextCol] === EMPTY) {
+        mapState[pacmanRow][pacmanCol] = EMPTY
+    }
+    else if(mapState[nextRow][nextCol] === R_GHOST || mapState[nextRow][nextCol] === B_GHOST) {
+        // gameOver()
+        console.log("GAME OVER")
+    }
+    mapState[pacmanRow][pacmanCol] = EMPTY
+    pacmanPosition = [nextRow, nextCol]
+    mapState[nextRow][nextCol] = PACMAN;
+    // drawPacman(pacmanPosition)
+    // drawScore()    
 
-//     var aPosition = gl.getAttribLocation(program, 'position');
-//     gl.enableVertexAttribArray(aPosition);
-//     gl.vertexAttribPointer(aPosition, vertCount, gl.FLOAT, false, 0, 0);
+}
 
-//     gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
-// }
-
+function movePacman(pacmanDirection) {
+    console.log(pacmanDirection)
+    if(pacmanDirection === UP) {
+        console.log('up')
+    }
+    checkCollision(pacmanDirection)
+    
+}
 async function setup() {
     // TODO: Initialize the context.
     initializeContext();
@@ -158,47 +197,57 @@ async function setup() {
     await loadShaders();
     compileShaders();
 
-    render();
+    // drawBackground();
+    startGame();
 };
 
 window.onload = setup;
 
+function drawBackground() {
+    // Clear the screen (for COLOR_BUFFER_BIT)
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-// Creates buffers using provided data.
-function createBuffers() {
-    // TODO: Create a position buffer for the vertices.
-    // In WebGL, the default winding order is counter-clock-wise,
-    // meaning that the order of vertices in a triangle must occur
-    // in a counter-clock-wise sequence relative to the viewer to be
-    // considered front-facing.
-    position_buffer = gl.createBuffer();
+    // Set the rendering st`ate to use the shader program
+    gl.useProgram(prog);
 
-    // TODO: Bind the buffer as an ARRAY_BUFFER to tell WebGL it will
-    // be used as a vertex buffer. Note that if another buffer was previously
-    // bound to ARRAY_BUFFER, that binding will be broken.
-    gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
+    // Create the vertex buffer and color buffer
+    var vertexBuffer = gl.createBuffer();
+    var colorBuffer = gl.createBuffer();
+    
+    if (!vertexBuffer || !colorBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+    // Loop through the mapState array
+    for (var row = 0; row < NUM_ROW; row++) {
+        for (var col = 0; col < NUM_COL; col++) {
+            var tile = mapState[row][col];
 
-    // TODO: Set the buffer data of the buffer bound to target 
-    // ARRAY_BUFFER with STATIC_DRAW usage. The usage is a hint
-    // that tells the API & driver the expected usage pattern of the backing
-    // data store. This allows it to make some optimizations.
-    gl.bufferData(gl.ARRAY_BUFFER,
-        new Float32Array(positions),
-        gl.STATIC_DRAW);
+            // Calculate the position of the current tile in clip space
+            var x = -1 + (2*(col)+1) * tileWidth / 2;
+            var y = 1 - (2*(row)+1) * tileHeight / 2;
 
-    // TODO: Repeat for the color vertex data.
-    color_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER,
-        new Float32Array(colors),
-        gl.STATIC_DRAW);
+            // Set the color based on the tile type
+            var color;
+            if (tile === WALL) {
+                color = GREEN;
+            } else {
+                color = GREY; // Black color for other tiles
+            }
 
-    logMessage("Created buffers.");
+            // Draw a rectangle for the background
+            drawRectangle(x, y, tileWidth, tileWidth, color, vertexBuffer, colorBuffer);
+        }
+    }
+    
+    // Delete the vertex buffer and color buffer
+    gl.deleteBuffer(vertexBuffer);
+    gl.deleteBuffer(colorBuffer);
+
 }
 
 
-// Draws the vertex data.
-function render() {
+function startGame() {
     // Clear the screen (for COLOR_BUFFER_BIT)
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -214,65 +263,67 @@ function render() {
         return -1;
     }
 
-    var pointsVertices = [
-        -0.5, -0.5
-    ]
-    var linesVertices = [
-        -0.25, -0.25,  -0.5, +0.5
-    ]
-    var triangleVertices = [
-        +0.5, -0.5,  0.0, 0.25,  +0.5, 0.0
-    ]
-    var circleVertices = []
+    // var pointsVertices = [
+    //     -0.5, -0.5
+    // ]
+    // var linesVertices = [
+    //     -0.25, -0.25,  -0.5, +0.5
+    // ]
+    // var triangleVertices = [
+    //     +0.5, -0.5,  0.0, 0.25,  +0.5, 0.0
+    // ]
 
-    
-    var circleColor = []
-    var radius = 0.3;
-    var centerX = -0.3;
-    var centerY = -0.3;
-     
-    for (var i=0.0; i<=360; i+=1) {
-        // degrees to radians
-        var j = i * Math.PI / 180;
-        // X Y Z
-        var vert1 = [
-          centerX+radius*Math.sin(j),
-          centerY+radius*Math.cos(j),
-        ];
-        var vert2 = [
-          centerX,
-          centerY,
-        ];
-        circleVertices = circleVertices.concat(vert1);
-        circleVertices = circleVertices.concat(vert2);
-        circleColor = circleColor.concat([0.0, 0.0, 1.0])
-        circleColor = circleColor.concat([0.0, 0.0, 1.0])
+
+    // // Draw points
+    // drawA(gl.POINTS, pointsVertices, colors, vertexBuffer, colorBuffer);
+
+    // // Draw lines
+    // drawA(gl.LINES, linesVertices, colors, vertexBuffer, colorBuffer);
+    // // Draw Circle
+    // drawCircle(0.32, 0, 0, DARK_YELLOW, vertexBuffer, colorBuffer)
+    // drawRectangle(0,0, 0.5, 0.5,BLUE, vertexBuffer, colorBuffer)
+    // drawCustomTriangle(0, 0, 0.3, 0.3, [1,0,0], vertexBuffer, colorBuffer)
+
+    // // Draw triangles
+    // drawA(gl.TRIANGLES, triangleVertices, colors, vertexBuffer, colorBuffer);
+
+    // Loop through the mapState array
+    for (var row = 0; row < NUM_ROW; row++) {
+        for (var col = 0; col < NUM_COL; col++) {
+            var tile = mapState[row][col];
+
+            // Calculate the position of the current tile in clip space
+            var x = -1 + (2*(col)+1) * tileWidth / 2;
+            var y = 1 - (2*(row)+1) * tileHeight / 2;
+            
+
+            if (tile === WALL) {
+                drawRectangle(x, y, tileWidth, tileWidth, GREEN, vertexBuffer, colorBuffer);
+            } else if (tile === DOT) {
+                drawRectangle(x, y, tileWidth, tileWidth, GREY, vertexBuffer, colorBuffer);
+                drawCircle(tileWidth/7, x, y, DARK_YELLOW, vertexBuffer, colorBuffer);
+            } else if (tile === PACMAN) {
+                drawRectangle(x, y, tileWidth, tileWidth, GREY, vertexBuffer, colorBuffer);
+                drawCustomTriangle(x,y, tileWidth/2, tileWidth/2, BLUE, vertexBuffer, colorBuffer);
+            } else if (tile === R_GHOST) {
+                drawRectangle(x, y, tileWidth, tileWidth, GREY, vertexBuffer, colorBuffer);
+                drawRectangle(x, y, tileWidth/2, tileWidth/2, RED, vertexBuffer, colorBuffer);
+            } else if (tile === B_GHOST){ 
+                drawRectangle(x, y, tileWidth, tileWidth, GREY, vertexBuffer, colorBuffer);
+                drawRectangle(x, y, tileWidth/2, tileWidth/2, TEAL_BLUE, vertexBuffer, colorBuffer);
+            } else {
+                drawRectangle(x, y, tileWidth, tileWidth, GREY, vertexBuffer, colorBuffer);
+            }
+            
+
+        }
     }
-    var colors = [
-        1, 0, 1, 
-        1, 0, 1, 
-        1, 0, 1// red
-    ]
-
-    // Draw points
-    drawA(gl.POINTS, pointsVertices, colors, vertexBuffer, colorBuffer);
-
-    // Draw lines
-    drawA(gl.LINES, linesVertices, colors, vertexBuffer, colorBuffer);
-    // Draw Circle
-    drawCircle(0.32, 0, 0, [0,0,1], vertexBuffer, colorBuffer)
-    drawRectangle(0,0, 0.5, 0.5, [0,1,1], vertexBuffer, colorBuffer)
-    drawCustomTriangle(0, 0, 0.3, 0.3, [1,0,0], vertexBuffer, colorBuffer)
-
-    // Draw triangles
-    drawA(gl.TRIANGLES, triangleVertices, colors, vertexBuffer, colorBuffer);
-
     
     // Delete the vertex buffer and color buffer
     gl.deleteBuffer(vertexBuffer);
     gl.deleteBuffer(colorBuffer);
     // TODO: Call this function repeatedly with requestAnimationFrame.
-    requestAnimationFrame(render);
+    requestAnimationFrame(startGame);
 }
 
  // Generic format
@@ -319,40 +370,13 @@ function render() {
 function setEventListeners(canvas) {
     canvas.addEventListener('keydown', function (event) {
         document.getElementById("keydown").innerText = event.key;
+        movePacman(event.key);
     });
 
-    canvas.addEventListener('keyup', function (event) {
-        document.getElementById("keyup").innerText = event.key;
-    });
-
-    canvas.addEventListener('mousemove', function (event) {
-        document.getElementById("mpos_x").innerText = event.x;
-        document.getElementById("mpos_y").innerText = event.y;
-    });
-
-    var click_count = 0;
-    canvas.addEventListener('click', function (event) {
-        click_count += 1;
-        document.getElementById("click_count").innerText = click_count;
-    })
+    // canvas.addEventListener('keyup', function (event) {
+    //     document.getElementById("keyup").innerText = event.key;
+    // });
 }
-
-
-// Logging
-
-function logMessage(message) {
-    document.getElementById("messageBox").innerText += `[msg]: ${message}\n`;
-}
-
-function logError(message) {
-    document.getElementById("messageBox").innerText += `[err]: ${message}\n`;
-}
-
-function logObject(obj) {
-    let message = JSON.stringify(obj, null, 2);
-    document.getElementById("messageBox").innerText += `[obj]:\n${message}\n\n`;
-}
-
 
 // Shader sources
 var vs_source;
@@ -378,10 +402,7 @@ async function loadShaders() {
     vs_source = shader_files[0];
     fs_source = shader_files[1];
 
-    // logMessage(vs_source);
-    // logMessage(fs_source);
-
-    logMessage("Shader files loaded.")
+    console.log("Shader files loaded.")
 }
 
 // Shader handles
@@ -402,7 +423,7 @@ function compileShaders() {
     // This can be done using the getShaderParameter function.
     // The error message can be retrieved with getShaderInfoLog.
     if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
-        logError(gl.getShaderInfoLog(vs));
+        console.error(gl.getShaderInfoLog(vs))
         gl.deleteShader(vs);
     }
 
@@ -412,7 +433,7 @@ function compileShaders() {
     gl.compileShader(fs);
 
     if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
-        logError(gl.getShaderInfoLog(fs));
+        console.error(gl.getShaderInfoLog(fs))
         gl.deleteShader(fs);
     }
 
@@ -432,8 +453,7 @@ function compileShaders() {
 
     // TODO: Check the LINK_STATUS using getProgramParameter
     if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
-        logError(gl.getProgramInfoLog(prog));
+        console.error(gl.getProgramInfoLog(prog));
     }
-
-    logMessage("Shader program compiled successfully.");
+    console.log("Shader program compiled successfully.");
 }
